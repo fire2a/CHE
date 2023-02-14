832259be926ce41b9710a04f1187f9902172f212
Rodrigo Mahaluf Recasens <rmahaluf@gmail.com>
2023-02-14 17:12:10 -0300

    just initializing

---
+++ elliptic_envelopes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 17:10:14 2023
+
+@author: rodrigo
+"""
+
---
+++ plot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 17:11:19 2023
+
+@author: rodrigo
+"""
+
